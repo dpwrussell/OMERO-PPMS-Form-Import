ffba81e4ae0843f5c6ba019de72a5107a2a19f21
dpwrussell <douglas_russell@hms.harvard.edu>
2016-05-05 00:36:42 -0400

    Search for datasets with session ID and write key-values to omero

--- monitor.py
+++ monitor.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
-# from omero_basics import OMEROConnectionManager
+from omero_basics import OMEROConnectionManager
+import omero
 import requests
 import re
 
-# conn_manager = OMEROConnectionManager()
-#
-# # TODO Perhaps exclude datasets that already have the data extracted from the
-# # PPMS form
-# q = """
-#     select dataset.id,
-#            dataset.name,
-#            dataset.description
-#     from Dataset dataset
-#     """
-#
-# # Run the query
-# rows = conn_manager.hql_query(q)
-#
-# for row in rows:
-#     print(row)
+conn_manager = OMEROConnectionManager()
 
+# TODO Perhaps exclude datasets that already have the data extracted from the
+# PPMS form. How do we know which ones are already processed?
+q = """
+    SELECT dataset.id,
+           dataset.description
+    FROM Dataset dataset
+    WHERE dataset.description LIKE '%Session #%'
+    """
+
+# Run the query
+rows = conn_manager.hql_query(q)
+
+for row in rows:
+    print(row)
+    datasetId = row[0]
+    sessionId = re.search(r'Session #([0-9]*)', row[1])
+    print sessionId.group(1)
+exit(1)
 # curl -d "action=GetSessionDetails&sessionid=12589&apikey=br8RfjAT7Bto4phxyfN5SKEaAetnmyDd&coreid=2" https://ppms.us/hms-lsp/API2/
 # curl -d "action=GetBookingFormContent&sessionid=12589&apikey=br8RfjAT7Bto4phxyfN5SKEaAetnmyDd&coreid=2" https://ppms.us/hms-lsp/API2/
 
@@ -61,8 +65,12 @@ with open("form.txt") as f:
     content = f.read()
     # content = item['content']
 
+    pairs = []
+
     m = re.split(u'^[!]{3}.+$', content, flags=re.MULTILINE)
     for i in m:
+
+
         # print i
 
         # Get the key and value of each item
@@ -71,30 +79,48 @@ with open("form.txt") as f:
         # kv = re.search(u'^(.+?):(?:\n|\r\n?)\[(.*)\]', i, flags=re.MULTILINE)
         k = re.search(r'^(.+?):', i, flags=re.MULTILINE)
         if k:
-            print k.group(1)
+            # print k.group(1)
+            key = k.group(1)
 
             # Look for square bracketed responses. If there are multiple then
             # these are checkboxes
             v = re.findall(r'^\[(.+)\][\b]*(.*)', i, flags=re.MULTILINE)
             if v and len(v) > 1:
                 for c in v:
-                    print "\tc %s" % c[1].strip()
-                    # if c.group(1) != 'left empty':
-                    #     print c.group(2)
+                    if c[0] == 'x':
+                        # print "\tc %s" % c[1].strip()
+                        value = c[1].strip()
+                        pairs.append([key, value])
 
             elif len(v) == 1:
                 if v[0][0].strip() != 'left empty':
-                    print "\t%s" % v[0][0].strip()
-                else:
-                    print '\tNULL'
+                    # print "\t%s" % v[0][0].strip()
+                    value = v[0][0].strip()
+                    pairs.append([key, value])
+                # else:
+                    # print '\tEmpty'
 
             else:
                 v = re.search(r'^\(x\)[\b]*(.*)', i, flags=re.MULTILINE)
                 if v:
-                    print '\ts %s' % v.group(1).strip()
+                    # print '\ts %s' % v.group(1).strip()
+                    value = v.group(1).strip()
+                    pairs.append([key, value])
 
+    for pair in pairs:
+        print pair
 
-
+    conn = conn_manager.connect()
+    conn.SERVICE_OPTS.setOmeroGroup(53)
+    mapAnn = omero.gateway.MapAnnotationWrapper(conn)
+    # Use 'client' namespace to allow editing in Insight & web
+    namespace = omero.constants.metadata.NSCLIENTMAPANNOTATION
+    mapAnn.setNs(namespace)
+    mapAnn.setValue(pairs)
+    mapAnn.save()
+    dataset = conn.getObject("Dataset", 151)
+    # NB: only link a client map annotation to a single object
+    dataset.linkAnnotation(mapAnn)
 
         # if kv:
         #     k = kv.group(1).strip()
